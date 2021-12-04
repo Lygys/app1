1b9864776abec9552836161130006879233bf9d3
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-04 02:52:23 +0000

    午前の作業終了

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+    @blog = Blog.new
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog),permit(:title, :category, :body)
+end
